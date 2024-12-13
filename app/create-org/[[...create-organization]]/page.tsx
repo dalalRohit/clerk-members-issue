b647dbe49b17efe005bed52e71044bc753995352
rohitDalalStrique <rohit.dalal@strique.io>
2024-12-13 16:04:10 +0530

    go back to main page link

--- app/create-org/[[...create-organization]]/page.tsx
+++ app/create-org/[[...create-organization]]/page.tsx
@@ -1,5 +1,11 @@
-import { CreateOrganization } from '@clerk/nextjs'
+import { CreateOrganization } from '@clerk/nextjs';
+import Link from 'next/link';
 
 export default function CreateOrganizationPage() {
-  return <CreateOrganization />
+  return (
+    <>
+      <Link href="/">Go back to main page</Link>
+      <CreateOrganization />
+    </>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 'use client';
-import {useOrganization} from '@clerk/nextjs';
+import { useOrganization } from '@clerk/nextjs';
 import Link from 'next/link';
 
 export default function Home() {
-  const {memberships}=useOrganization({
+  const { memberships } = useOrganization({
     memberships: {
       pageSize: 50
     },
     invitations: true
   });
-  const membersList=memberships?.data?.map(m => m.publicUserData);
-  console.log('membersList:', membersList);  
-  return <div>
-    <Link href="/create-org" >Create Org</Link>
-    <pre>{JSON.stringify(membersList,null,4)}</pre>
-  </div>;
+  const membersList = memberships?.data?.map((m) => m.publicUserData);
+  console.log('membersList:', membersList);
+  return (
+    <div>
+      <Link href="/create-org">Create Org</Link>
+      <pre>{JSON.stringify(membersList, null, 4)}</pre>
+    </div>
+  );
 }
